b4605246fc7a50b5902930944f070b0c6e01fefc
StaszekStepien <229852@student.pwr.edu.pl>
2021-03-28 18:56:23 +0200

    V 1.1: - search functionality - started working on saving the images

--- app/src/main/java/com/example/galleryapp229852/activity/MainActivity.kt
+++ app/src/main/java/com/example/galleryapp229852/activity/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.galleryapp229852.activity
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.preference.PreferenceManager
 import android.provider.MediaStore
 import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.helper.ItemTouchHelper
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.SearchView
 import android.widget.TextView
 import android.widget.Toast
 import com.example.galleryapp229852.R
@@ -22,9 +27,12 @@ import com.example.galleryapp229852.adapter.GalleryImageAdapter
 import com.example.galleryapp229852.adapter.GalleryImageClickListener
 import com.example.galleryapp229852.adapter.Image
 import com.example.galleryapp229852.fragment.GalleryFullscreenFragment
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.io.IOException
+import java.lang.reflect.Type
 import java.nio.file.Files.createFile
 import java.text.SimpleDateFormat
 import java.util.*
@@ -34,15 +42,16 @@ class MainActivity : AppCompatActivity(), GalleryImageClickListener {
 
     // gallery column count
     private val SPAN_COUNT = 1
-
     private var imageList = ArrayList<Image>()
+    var displayList = ArrayList<Image>()
     lateinit var galleryAdapter: GalleryImageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         // init adapter
-        galleryAdapter = GalleryImageAdapter(imageList)
+        galleryAdapter = GalleryImageAdapter(displayList)
         galleryAdapter.listener = this
         var itemTouchHelper = ItemTouchHelper(SwipeToDelete(galleryAdapter))
         itemTouchHelper.attachToRecyclerView(findViewById(R.id.recyclerView))
@@ -51,23 +60,27 @@ class MainActivity : AppCompatActivity(), GalleryImageClickListener {
         recyclerView.adapter = galleryAdapter
 
         // load images
+        loadData()
         loadImages()
     }
 
     private fun loadImages() {
+        if (imageList.isEmpty()) {
+            imageList.add(Image("https://i.ibb.co/wBYDxLq/beach.jpg", "Beach Houses"))
+            imageList.add(Image("https://i.ibb.co/gM5NNJX/butterfly.jpg", "Butterfly"))
+            imageList.add(Image("https://i.ibb.co/10fFGkZ/car-race.jpg", "Car Racing"))
+            imageList.add(Image("https://i.ibb.co/ygqHsHV/coffee-milk.jpg", "Coffee with Milk"))
+            imageList.add(Image("https://i.ibb.co/7XqwsLw/fox.jpg", "Fox"))
+            imageList.add(Image("https://i.ibb.co/L1m1NxP/girl.jpg", "Mountain Girl"))
+            imageList.add(Image("https://i.ibb.co/wc9rSgw/desserts.jpg", "Desserts Table"))
+            imageList.add(Image("https://i.ibb.co/wdrdpKC/kitten.jpg", "Kitten"))
+            imageList.add(Image("https://i.ibb.co/dBCHzXQ/paris.jpg", "Paris Eiffel"))
+            imageList.add(Image("https://i.ibb.co/JKB0KPk/pizza.jpg", "Pizza Time"))
+            imageList.add(Image("https://i.ibb.co/VYYPZGk/salmon.jpg", "Salmon "))
+            imageList.add(Image("https://i.ibb.co/JvWpzYC/sunset.jpg", "Sunset in Beach"))
 
-        imageList.add(Image("https://i.ibb.co/wBYDxLq/beach.jpg", "Beach Houses"))
-        imageList.add(Image("https://i.ibb.co/gM5NNJX/butterfly.jpg", "Butterfly"))
-        imageList.add(Image("https://i.ibb.co/10fFGkZ/car-race.jpg", "Car Racing"))
-        imageList.add(Image("https://i.ibb.co/ygqHsHV/coffee-milk.jpg", "Coffee with Milk"))
-        imageList.add(Image("https://i.ibb.co/7XqwsLw/fox.jpg", "Fox"))
-        imageList.add(Image("https://i.ibb.co/L1m1NxP/girl.jpg", "Mountain Girl"))
-        imageList.add(Image("https://i.ibb.co/wc9rSgw/desserts.jpg", "Desserts Table"))
-        imageList.add(Image("https://i.ibb.co/wdrdpKC/kitten.jpg", "Kitten"))
-        imageList.add(Image("https://i.ibb.co/dBCHzXQ/paris.jpg", "Paris Eiffel"))
-        imageList.add(Image("https://i.ibb.co/JKB0KPk/pizza.jpg", "Pizza Time"))
-        imageList.add(Image("https://i.ibb.co/VYYPZGk/salmon.jpg", "Salmon "))
-        imageList.add(Image("https://i.ibb.co/JvWpzYC/sunset.jpg", "Sunset in Beach"))
+        }
+        displayList.addAll(imageList)
         galleryAdapter.notifyDataSetChanged()
     }
 
@@ -75,7 +88,7 @@ class MainActivity : AppCompatActivity(), GalleryImageClickListener {
         // handle click of image
 
         val bundle = Bundle()
-        bundle.putSerializable("images", imageList)
+        bundle.putSerializable("images", displayList)
         bundle.putInt("position", position)
 
         val fragmentTransaction = supportFragmentManager.beginTransaction()
@@ -86,8 +99,38 @@ class MainActivity : AppCompatActivity(), GalleryImageClickListener {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
-        inflater.inflate(R.menu.camera_menu,menu)
-        return true
+        inflater.inflate(R.menu.camera_menu, menu)
+        val menuItem = menu!!.findItem(R.id.searchOption)
+        if (menuItem != null) {
+            val searchView = menuItem.actionView as SearchView
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    if (newText!!.isNotEmpty()) {
+                        displayList.clear()
+                        val search = newText.toLowerCase(Locale.getDefault())
+                        imageList.forEach {
+                            if (it.title.toLowerCase(Locale.getDefault()).contains(search)) {
+                                displayList.add(it)
+                            }
+                        }
+                        recyclerView.adapter!!.notifyDataSetChanged()
+
+                    } else {
+                        displayList.clear()
+                        displayList.addAll(imageList)
+                        recyclerView.adapter!!.notifyDataSetChanged()
+                    }
+                    return true
+                }
+
+
+            })
+        }
+        return super.onCreateOptionsMenu(menu)
     }
 
     private val REQUEST_IMAGE_CAPTURE = 1
@@ -100,7 +143,7 @@ class MainActivity : AppCompatActivity(), GalleryImageClickListener {
                 val photoFile: File? = try {
                     createImageFile()
                 } catch (ex: IOException) {
-                    Toast.makeText(this,ex.toString(),Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, ex.toString(), Toast.LENGTH_SHORT).show()
                     null
                 }
                 // Continue only if the File was successfully created
@@ -116,14 +159,45 @@ class MainActivity : AppCompatActivity(), GalleryImageClickListener {
             }
         }
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.camera_option){
+        if (item.itemId == R.id.camera_option) {
             dispatchTakePictureIntent()
             return true
         }
-
+        if (item.itemId == R.id.saveButton) {
+            saveData()
+            return true
+        }
         return super.onOptionsItemSelected(item)
     }
+
+    private fun saveData() {
+        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        //PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = sharedPreferences.edit()
+        val gson = Gson()
+        val json = gson.toJson(imageList)
+        editor.putString("task list", json)
+        editor.apply()
+        editor.commit()
+        val toast = Toast.makeText(applicationContext, "Saved!", Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+    private inline fun <reified T> genericType(): Type = object : TypeToken<T>() {}.type
+
+
+    private fun loadData() {
+        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        //PreferenceManager.getDefaultSharedPreferences(this)
+        val gson = Gson()
+        val json = sharedPreferences.getString("task list", null)
+        val type = genericType<ArrayList<Image>>()
+        imageList.clear()
+        imageList = gson.fromJson(json, type)
+    }
+
     private lateinit var currentPhotoPath: String
 
     @Throws(IOException::class)
@@ -140,17 +214,19 @@ class MainActivity : AppCompatActivity(), GalleryImageClickListener {
             currentPhotoPath = absolutePath
         }
     }
+
     private fun File.writeBitmap(bitmap: Bitmap, format: Bitmap.CompressFormat, quality: Int) {
         outputStream().use { out ->
             bitmap.compress(format, quality, out)
             out.flush()
         }
     }
+
     private fun galleryAddPic() {
         Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
             val f = File(currentPhotoPath)
             mediaScanIntent.data = Uri.fromFile(f)
-            imageList.add(Image(mediaScanIntent.data.toString(),currentPhotoPath))
+            displayList.add(Image(mediaScanIntent.data.toString(), currentPhotoPath))
 
             sendBroadcast(mediaScanIntent)
         }
@@ -158,7 +234,7 @@ class MainActivity : AppCompatActivity(), GalleryImageClickListener {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_IMAGE_CAPTURE && data != null){
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_IMAGE_CAPTURE && data != null) {
             galleryAddPic()
             //imageList.add(data.extras?.get("data") as Image)
             galleryAdapter.notifyDataSetChanged()
